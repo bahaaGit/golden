f139eb9437874de064f00b8bd2e081ed8870a5be
Farid Ramos <ramosf@purdue.edu>
2017-12-02 18:05:31 -0500

    finished front-ui

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('views', './views');
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) =>{
-	res.render('index');
+	res.render('index', {title: 'Golden Storage'.toUpperCase()});
 });
 
 app.listen(3000, () => {
