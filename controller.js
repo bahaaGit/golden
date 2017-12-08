577bd375fcf2592a5c96915618147e43318e791a
Farid Ramos <ramosf@purdue.edu>
2017-12-08 12:35:31 -0500

    added getMapURL

--- controller.js
+++ controller.js
@@ -4,6 +4,26 @@ var data = {
     town: 'West Lafayette, IN 47906',
     zipcode: '47906',
     host: 'Anthony Stark'
+    getMapUrl: function() {
+        var req = this.address;
+        req += ", ";
+        var temp = this.town.replace("-", "");
+        req += temp;
+        req = req.replace("  ", " ");
+        req = req.replace(/ /g, "+");
+        
+        var mapURL = "https://maps.googleapis.com/maps/api/staticmap?center=";
+        var apiKey = "&key=AIzaSyBCRzpLARuNB2qrxt2YEdzwGtwrEF-P-Ig"; // our API key
+        var params = "&zoom=15&size=350x200";
+        var markers = "&markers=size:mid%7Ccolor:red%7C";
+        markers += req;
+        
+        params += markers;
+        params += apiKey;
+        mapURL += req;
+        mapURL += params;
+        return mapURL;
+    }
 }
 for (var i = 0; i < 9; i++) {
     posts.push(data);
