0d51c1222895c041b9d37084d4a381acc4d7f530
Abdul Aziz Bah <abdulazizbah@ymail.com>
2017-12-08 12:32:07 -0500

    add  mongodb and database stuff

--- js/script.js
+++ js/script.js
@@ -1,46 +1,25 @@
-// Initialize Firebase
-/*var config = {
-    apiKey: "AIzaSyDvrvAVb8-u2m0k-eej1ykxW7ucqJxjpqo",
-    authDomain: "mygoldenstorage-deacf.firebaseapp.com",
-    databaseURL: "https://mygoldenstorage-deacf.firebaseio.com",
-    projectId: "mygoldenstorage-deacf",
-    storageBucket: "mygoldenstorage-deacf.appspot.com",
-    messagingSenderId: "58479308080"
-};
-firebase.initializeApp(config);*/
-
 $(document).ready(function() {
 
     $('#loginForm').on('submit', function() {
+        var userID = $('#userID');
+        var password = $('#password');
+        console.log(userID);
 
-        var userID = $('#loginForm #userID');
-        var password = $('#loginForm #password');
 
-        /*firebase.auth().signInWithEmailAndPassword(userID, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        });*/
-        //validate
-        //....
         var todo = { userID: userID.val(), password: password.val() };
 
         console.log(todo.userID, todo.password);
-        $.ajax({
+        /*$.ajax({
             type: 'POST',
             url: '/login',
             data: todo,
             success: function(data) {
-                //do something with the data via front-end framework
-                //console.log(data);
                 console.log("not inside");
-                //location.reload();
             },
             error: function(textStatus, errorThrown) {
                 console.log("am inside");
             }
-        });
+        });*/
 
         return false;
 
@@ -88,7 +67,6 @@ $(document).ready(function() {
 
     $('.textArea').on('click', function() {
         var post = $(this).text().replace();
-
         $.ajax({
             type: 'DELETE',
             url: '/hostprofile/' + item,
